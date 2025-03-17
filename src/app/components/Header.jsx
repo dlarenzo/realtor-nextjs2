2bb4de890984e9cc772992e8db44c7661851ce2c
LaRenzo <dlarenzo@outlook.com>
2025-03-17 09:17:32 -0600

    done with CRUD, moving frontend

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,37 @@
+"use client";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md z-20 fixed w-full">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link href="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
+            <span className="text-slate-500">Home</span>
+            <span className="text-yellow-500">Realty</span>
+          </h1>
+        </Link>
+
+        {/* NAVIGATION */}
+        <ul className="flex gap-4">
+          <Link href="/">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+
+          <Link href="/sign-in">
+            <li className="hidden md:inline text-slate-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
